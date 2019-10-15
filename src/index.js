cff504e2d28c849f9dbec135810089c4068998a7
Ashiqur Rahman <ashiquejava@gmail.com>
2019-10-16 02:18:48 +0600

    fix:very basic redux example

--- src/index.js
+++ src/index.js
@@ -2,11 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// STORE
+
+// ACTION
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+
+const store = createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment()); // 1
+store.dispatch(increment()); // 2
+store.dispatch(decrement()); // 3
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
